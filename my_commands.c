9fffe877bc1def066eb3cab19d622eeab9b767f3
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2026-02-21 23:54:45 -0800

    Added insert_nums_0 and insert_nums_1. Eventually it would be nice to have a generic insert_nums command that pops up an input dialog to the user to let them input a format string to specify "start" and "increment"

--- my_commands.c
+++ my_commands.c
@@ -16,3 +16,47 @@ DEF_PLUGIN_EDITOR_HOOK("Kill Line(s)", "Removes the entire line that the cursor 
 		ed_push_command(ctx, &command);
 	}
 }
+
+void InsertNums(i64 start, i64 increment)
+{
+	ScratchBegin(scratch);
+	
+	EditorCursorArray cursors = ZEROED;
+	ed_cursor_ranges(scratch, CtxGlobal, &cursors);
+	if (cursors.size == 0) { return; }
+	
+	EditorBatchReplace batchReplace = ZEROED;
+	batchReplace.size = cursors.size;
+	batchReplace.array = AllocArray(EditorReplaceData, scratch, batchReplace.size);
+	
+	u64 currentNumber = start;
+	for (u64 cIndex = 0; cIndex < cursors.size; cIndex++)
+	{
+		EditorCursorRange cursor = cursors.array[cIndex];
+		
+		// EditorOffsetRange cursorOffsetRange = { .first_off = cursor.sel.first_off, .last_off = cursor.sel.last_off };
+		// Str8 cursorContents = ZEROED;
+		// ed_string_at_range(scratch, CtxGlobal, &cursorOffsetRange, &cursorContents);
+		// NotifyPrint_W("Cursor[%llu]: \"%S\"", cIndex, cursorContents);
+		
+		batchReplace.array[cIndex].range = (EditorOffsetRange){ .first_off = cursor.sel.first_off, .last_off = cursor.sel.last_off };
+		batchReplace.array[cIndex].buf = PrintInArena(scratch, "%llu", currentNumber);
+		currentNumber += increment;
+	}
+	
+	EditorBatchEdit batchEdit = ZEROED;
+	ed_edit_batch_begin(CtxGlobal, &batchEdit);
+	ed_edit_batch_replace(&batchEdit, &batchReplace);
+	ed_edit_batch_end(CtxGlobal, &batchEdit);
+	
+	ScratchEnd(scratch);
+}
+
+DEF_PLUGIN_EDITOR_HOOK("Insert Numbers (from 0)", "Inserts ascending numbers (starting at 0) for each multi-cursor", insert_nums_0)
+{
+	WithContext(ctx) { InsertNums(0, 1); }
+}
+DEF_PLUGIN_EDITOR_HOOK("Insert Numbers (from 1)", "Inserts ascending numbers (starting at 1) for each multi-cursor", insert_nums_1)
+{
+	WithContext(ctx) { InsertNums(1, 1); }
+}
