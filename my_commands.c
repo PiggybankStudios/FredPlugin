37da83e8e632094855f8e2b3e3fbf607372d9757
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2026-02-27 19:04:58 -0800

    Finished the `toggle_header` command

--- my_commands.c
+++ my_commands.c
@@ -147,49 +147,41 @@ DEF_PLUGIN_EDITOR_HOOK("Align cursors", "Inserts spaces at each cursor until all
 // +==============================+
 // |        toggle_header         |
 // +==============================+
-Str8 GetHeaderLinesForTitle(Arena* arena, Str8 title, Str8 lineCommentSyntax, u64 width, char cornerChar, char topChar, char bottomChar, char leftChar, char rightChar)
+//TODO: This code is quite verbose and unweildy, we should probably make some helper functions that make it cleaner (most string manipulation)
+Str8 ConvertTitleToHeader(Arena* arena, Str8 title, Str8 indentationStr, Str8 lineCommentSyntax, u64 width, char cornerChar, char topChar, char bottomChar, char leftChar, char rightChar)
 {
 	Str8 result = Str8_Empty;
 	ScratchBegin1(scratch, arena);
 	
 	u64 actualWidth = Max2(width, title.size + 4);
-	Str8 lineIndentation = Str8_Empty;
-	for (u64 charIndex = 0; charIndex < title.size; charIndex++)
-	{
-		if (title.str[charIndex] == ' ' || title.str[charIndex] == '\t')
-		{
-			lineIndentation = StrSlice(title, 0, charIndex);
-		}
-		else { break; }
-	}
 	Str8 line1 = Str8_Empty;
 	Str8 line2 = Str8_Empty;
 	Str8 line3 = Str8_Empty;
 	line1.size = lineCommentSyntax.size + actualWidth;
-	line2.size = lineIndentation.size + lineCommentSyntax.size + actualWidth;
-	line3.size = lineIndentation.size + lineCommentSyntax.size + actualWidth;
+	line2.size = indentationStr.size + lineCommentSyntax.size + actualWidth;
+	line3.size = indentationStr.size + lineCommentSyntax.size + actualWidth;
 	line1.str = AllocArray(char, scratch, line1.size);
 	line2.str = AllocArray(char, scratch, line2.size);
 	line3.str = AllocArray(char, scratch, line3.size);
 	
 	u64 textOffset = actualWidth/2 - title.size/2;
 	
-	if (lineIndentation.size > 0)
+	if (indentationStr.size > 0)
 	{
-		memcpy(&line2.str[0], lineIndentation.str, lineIndentation.size);
-		memcpy(&line3.str[0], lineIndentation.str, lineIndentation.size);
+		memcpy(&line2.str[0], indentationStr.str, indentationStr.size);
+		memcpy(&line3.str[0], indentationStr.str, indentationStr.size);
 	}
 	if (lineCommentSyntax.size > 0)
 	{
-		memcpy(&line1.str[lineIndentation.size], lineCommentSyntax.str, lineCommentSyntax.size);
-		memcpy(&line2.str[lineIndentation.size], lineCommentSyntax.str, lineCommentSyntax.size);
-		memcpy(&line3.str[lineIndentation.size], lineCommentSyntax.str, lineCommentSyntax.size);
+		memcpy(&line1.str[0], lineCommentSyntax.str, lineCommentSyntax.size);
+		memcpy(&line2.str[indentationStr.size], lineCommentSyntax.str, lineCommentSyntax.size);
+		memcpy(&line3.str[indentationStr.size], lineCommentSyntax.str, lineCommentSyntax.size);
 	}
 	for (u64 charIndex = 0; charIndex < actualWidth; charIndex++)
 	{
-		char* lineChar1 = &line1.str[lineIndentation.size + lineCommentSyntax.size + charIndex];
-		char* lineChar2 = &line2.str[lineIndentation.size + lineCommentSyntax.size + charIndex];
-		char* lineChar3 = &line3.str[lineIndentation.size + lineCommentSyntax.size + charIndex];
+		char* lineChar1 = &line1.str[lineCommentSyntax.size + charIndex];
+		char* lineChar2 = &line2.str[indentationStr.size + lineCommentSyntax.size + charIndex];
+		char* lineChar3 = &line3.str[indentationStr.size + lineCommentSyntax.size + charIndex];
 		
 		if (charIndex == 0 || charIndex == actualWidth-1) { *lineChar1 = cornerChar; *lineChar3 = cornerChar; }
 		else { *lineChar1 = topChar; *lineChar3 = bottomChar; }
@@ -218,9 +210,14 @@ bool IsValidTitleChar(char c)
 	if (c >= 'a' && c <= 'z') { return true; }
 	if (c >= '0' && c <= '9') { return true; }
 	if (c == ' ') { return true; }
+	if (c == ',' || c == '\'' || c == '/' || c == '\\' ||
+	    c == '(' || c == ')' || c == '-' || c == '_' || c == '|')
+	{
+		return true;
+	}
 	return false;
 }
-bool IsLinePartOfHeader(Str8 line, Str8 lineCommentSyntax, Str8* titleOut)
+bool IsStrPartOfHeader(Str8 lineStr, Str8 lineCommentSyntax, Str8* titleOut, Str8* indentationOut)
 {
 	SetOptionalOutPntr(titleOut, Str8_Empty);
 	bool foundCommentSyntax = false;
@@ -229,25 +226,28 @@ bool IsLinePartOfHeader(Str8 line, Str8 lineCommentSyntax, Str8* titleOut)
 	bool foundSecondCornerOrSide = false;
 	u64 charIndex = 0;
 	
-	while (charIndex < line.size && (line.str[charIndex] == ' ' || line.str[charIndex] == '\t')) { charIndex++; }
+	//Eat indentation
+	Str8 indentationStr = StrSlice(lineStr, 0, 0);
+	while (charIndex < lineStr.size && (lineStr.str[charIndex] == ' ' || lineStr.str[charIndex] == '\t')) { charIndex++; indentationStr.size++; }
+	SetOptionalOutPntr(indentationOut, indentationStr);
 	
-	if (line.size - charIndex < lineCommentSyntax.size) { return false; }
-	if (memcmp(&line.str[charIndex], lineCommentSyntax.str, lineCommentSyntax.size) != 0) { return false; }
+	if (lineStr.size - charIndex < lineCommentSyntax.size) { return false; }
+	if (memcmp(&lineStr.str[charIndex], lineCommentSyntax.str, lineCommentSyntax.size) != 0) { return false; }
 	charIndex += lineCommentSyntax.size;
 	
 	// Does it look like a center line of a header? (with '|' and spaces on either side)
-	if (line.size - charIndex >= 5 &&
-		line.str[charIndex] == '|' && line.str[charIndex+1] == ' ' &&
-		line.str[line.size-2] == ' ' && line.str[line.size-1] == '|')
+	if (lineStr.size - charIndex >= 5 &&
+		lineStr.str[charIndex] == '|' && lineStr.str[charIndex+1] == ' ' &&
+		lineStr.str[lineStr.size-2] == ' ' && lineStr.str[lineStr.size-1] == '|')
 	{
 		charIndex += 2;
 		
-		while (charIndex < line.size && line.str[charIndex] == ' ') { charIndex++; }
-		if (charIndex >= line.size-2) { return false; }
+		while (charIndex < lineStr.size && lineStr.str[charIndex] == ' ') { charIndex++; }
+		if (charIndex >= lineStr.size-2) { return false; }
 		
-		if (titleOut != nullptr) { titleOut->str = &line.str[charIndex]; }
-		while (charIndex < line.size && IsValidTitleChar(line.str[charIndex])) { charIndex++; if (titleOut != nullptr) { titleOut->size++; } }
-		if (charIndex < line.size-2) { return false; }
+		if (titleOut != nullptr) { titleOut->str = &lineStr.str[charIndex]; }
+		while (charIndex < lineStr.size-1 && IsValidTitleChar(lineStr.str[charIndex])) { charIndex++; if (titleOut != nullptr) { titleOut->size++; } }
+		if (charIndex < lineStr.size-2) { return false; }
 		
 		if (titleOut != nullptr)
 		{
@@ -257,16 +257,53 @@ bool IsLinePartOfHeader(Str8 line, Str8 lineCommentSyntax, Str8* titleOut)
 		return true;
 	}
 	// Does it look like a top/bottom line of a header? Where it has corner chars '+' and has a single repeated char in-between?
-	else if (line.size - charIndex >= 3 &&
-		line.str[charIndex] == '+' && line.str[line.size-1] == '+')
+	else if (lineStr.size - charIndex >= 3 &&
+		lineStr.str[charIndex] == '+' && lineStr.str[lineStr.size-1] == '+')
 	{
 		charIndex++;
-		char topChar = line.str[charIndex];
-		while (charIndex < line.size-1 && line.str[charIndex] == topChar) { charIndex++; }
-		if (charIndex < line.size-1) { return false; } //not all the chars were the same topChar
+		char topChar = lineStr.str[charIndex];
+		while (charIndex < lineStr.size-1 && lineStr.str[charIndex] == topChar) { charIndex++; }
+		if (charIndex < lineStr.size-1) { return false; } //not all the chars were the same topChar
 		return true;
 	}
 	else { return false; }
+}
+bool FindFullHeaderAroundLine(u64 line, Str8 lineCommentSyntax, Arena* lineArena, u64* firstLineOut, Str8* titleOut, Str8* indentationOut)
+{
+	ScratchBegin1(scratch, lineArena);
+	u64 numLinesInFile = ed_line_count(Ctx);
+	for (u64 lineIndex = (line > 2 ? line-2 : 0); lineIndex <= line; lineIndex++)
+	{
+		if (lineIndex + 2 > numLinesInFile) { continue; } //TODO: Equal to?
+		
+		Str8 firstLineTitle = Str8_Empty;
+		EditorOffsetRange firstLineRange = ZEROED;
+		ed_byte_range_at_line(Ctx, lineIndex + 0, &firstLineRange);
+		Str8 firstLineStr = Str8_Empty;
+		ed_string_at_range(scratch, Ctx, &firstLineRange, &firstLineStr);
+		if (!IsStrPartOfHeader(firstLineStr, lineCommentSyntax, &firstLineTitle, indentationOut) || firstLineTitle.size > 0) { continue; }
+		
+		Str8 middleLineTitle = Str8_Empty;
+		EditorOffsetRange middleLineRange = ZEROED;
+		ed_byte_range_at_line(Ctx, lineIndex + 1, &middleLineRange);
+		Str8 middleLineStr = Str8_Empty;
+		ed_string_at_range(scratch, Ctx, &middleLineRange, &middleLineStr);
+		if (!IsStrPartOfHeader(middleLineStr, lineCommentSyntax, &middleLineTitle, nullptr) || middleLineTitle.size == 0) { continue; }
+		
+		Str8 lastLineTitle = Str8_Empty;
+		EditorOffsetRange lastLineRange = ZEROED;
+		ed_byte_range_at_line(Ctx, lineIndex + 2, &lastLineRange);
+		Str8 lastLineStr = Str8_Empty;
+		ed_string_at_range(scratch, Ctx, &lastLineRange, &lastLineStr);
+		if (!IsStrPartOfHeader(lastLineStr, lineCommentSyntax, &lastLineTitle, nullptr) || lastLineTitle.size > 0) { continue; }
+		
+		SetOptionalOutPntr(firstLineOut, lineIndex);
+		SetOptionalOutPntr(titleOut, AllocStr8(lineArena, middleLineTitle));
+		ScratchEnd(scratch);
+		return true;
+	}
+	ScratchEnd(scratch);
+	return false;
 }
 void ToggleHeader(Str8 lineCommentSyntax, u64 width, char cornerChar, char topChar, char bottomChar, char leftChar, char rightChar)
 {
@@ -287,84 +324,12 @@ void ToggleHeader(Str8 lineCommentSyntax, u64 width, char cornerChar, char topCh
 		ed_string_at_range(scratch, Ctx, &cursor.sel, &cursorContents);
 		u64 cursorLine = ed_line_at_offset(Ctx, Min2(cursor.sel.first_off, cursor.sel.last_off));
 		
-		bool isCursorOnHeader = false;
 		Str8 headerText = Str8_Empty;
+		Str8 indentationStr = Str8_Empty;
 		u64 headerTopLine = 0;
-		
-		do //breakable block TODO: Make a macro for this?
+		if (FindFullHeaderAroundLine(cursorLine, lineCommentSyntax, scratch, &headerTopLine, &headerText, &indentationStr))
 		{
-			EditorOffsetRange lineRange = ZEROED;
-			ed_byte_range_at_line(Ctx, cursorLine + 0, &lineRange);
-			Str8 lineStr = Str8_Empty;
-			ed_string_at_range(scratch, Ctx, &lineRange, &lineStr);
-			if (IsLinePartOfHeader(lineStr, lineCommentSyntax, &headerText))
-			{
-				// If this was a top or bottom line we gotta look up/down 2 lines and see if the entire header is there
-				if (headerText.size == 0)
-				{
-					if (cursorLine >= 2)
-					{
-						EditorOffsetRange firstLineRange = ZEROED;
-						ed_byte_range_at_line(Ctx, cursorLine - 2, &firstLineRange);
-						Str8 firstLineStr = Str8_Empty;
-						ed_string_at_range(scratch, Ctx, &firstLineRange, &firstLineStr);
-						EditorOffsetRange middleLineRange = ZEROED;
-						ed_byte_range_at_line(Ctx, cursorLine - 1, &middleLineRange);
-						Str8 middleLineStr = Str8_Empty;
-						ed_string_at_range(scratch, Ctx, &middleLineRange, &middleLineStr);
-						
-						if (IsLinePartOfHeader(middleLineStr, lineCommentSyntax, &headerText) && headerText.size > 0)
-						{
-							Str8 topLineHeaderText = Str8_Empty;
-							if (IsLinePartOfHeader(firstLineStr, lineCommentSyntax, &topLineHeaderText) && topLineHeaderText.size == 0)
-							{
-								isCursorOnHeader = true;
-								headerTopLine = cursorLine-2;
-							}
-							else { Notify_W("Line-2 is not a valid header top"); break; }
-						}
-						else { Notify_W("Line-1 is not a valid header center"); break; }
-					}
-					else if (cursorLine <= ed_line_count(Ctx) - 2)
-					{
-						EditorOffsetRange middleLineRange = ZEROED;
-						ed_byte_range_at_line(Ctx, cursorLine + 1, &middleLineRange);
-						Str8 middleLineStr = Str8_Empty;
-						ed_string_at_range(scratch, Ctx, &middleLineRange, &middleLineStr);
-						EditorOffsetRange bottomLineRange = ZEROED;
-						ed_byte_range_at_line(Ctx, cursorLine + 2, &bottomLineRange);
-						Str8 bottomLineStr = Str8_Empty;
-						ed_string_at_range(scratch, Ctx, &bottomLineRange, &bottomLineStr);
-						
-						if (IsLinePartOfHeader(middleLineStr, lineCommentSyntax, &headerText) && headerText.size > 0)
-						{
-							Str8 bottomLineHeaderText = Str8_Empty;
-							if (IsLinePartOfHeader(bottomLineStr, lineCommentSyntax, &bottomLineHeaderText) && bottomLineHeaderText.size == 0)
-							{
-								isCursorOnHeader = true;
-								headerTopLine = cursorLine;
-							}
-							else { Notify_W("Line+1 is not a valid header center"); break; }
-						}
-						else { Notify_W("Line+2 is not a valid header bottom"); break; }
-					}
-					else { Notify_W("Not enough surrounding lines"); break; }
-				}
-				// If this is the middle line then we just need to check that the line above and below are header lines
-				else if (cursorLine > 0)
-				{
-					//TODO: Check lines above and below to see if they are part of the header
-					// isCursorOnHeader = true;
-					// headerTopLine = cursorLine-1;
-					Notify_W("Unimplemented");
-				}
-			}
-		} while(false);
-		
-		
-		// Replace the entirety of the 3-lines with just the headerText
-		if (isCursorOnHeader)
-		{
+			// Replace the entirety of the 3-lines with just the headerText
 			EditorOffsetRange topLineRange = ZEROED;
 			ed_byte_range_at_line(Ctx, headerTopLine, &topLineRange);
 			EditorOffsetRange bottomLineRange = ZEROED;
@@ -375,9 +340,14 @@ void ToggleHeader(Str8 lineCommentSyntax, u64 width, char cornerChar, char topCh
 			};
 			
 			NotifyPrint_I("Collapsing header on lines %llu-%llu to \"%S\"", headerTopLine, headerTopLine+2, headerText);
+			Str8 headerTextWithIndentation = Str8_Empty;
+			headerTextWithIndentation.size = indentationStr.size + headerText.size;
+			headerTextWithIndentation.str = AllocArray(char, scratch, headerTextWithIndentation.size);
+			if (indentationStr.size > 0) { memcpy(&headerTextWithIndentation.str[0], indentationStr.str, indentationStr.size); }
+			memcpy(&headerTextWithIndentation.str[indentationStr.size], headerText.str, headerText.size);
 			#if 1
 			batchReplace.array[replaceIndex].range = headerRange;
-			batchReplace.array[replaceIndex].buf = headerText;
+			batchReplace.array[replaceIndex].buf = headerTextWithIndentation;
 			replaceIndex++;
 			#else
 			EditorCmd cmd = ZEROED;
@@ -404,8 +374,14 @@ void ToggleHeader(Str8 lineCommentSyntax, u64 width, char cornerChar, char topCh
 		}
 		else if (cursorContents.size > 0)
 		{
+			EditorOffsetRange cursorLineRange = ZEROED;
+			ed_byte_range_at_line(Ctx, cursorLine, &cursorLineRange);
+			Str8 cursorLineStr = Str8_Empty;
+			ed_string_at_range(scratch, Ctx, &cursorLineRange, &cursorLineStr);
+			indentationStr = StrSlice(cursorLineStr, 0, 0);
+			while (indentationStr.size < cursorLineStr.size && (cursorLineStr.str[indentationStr.size] == ' ' || cursorLineStr.str[indentationStr.size] == '\t')) { indentationStr.size++; }
 			// Create the header
-			Str8 headerLines = GetHeaderLinesForTitle(scratch, cursorContents, lineCommentSyntax, width, cornerChar, topChar, bottomChar, leftChar, rightChar);
+			Str8 headerLines = ConvertTitleToHeader(scratch, cursorContents, indentationStr, lineCommentSyntax, width, cornerChar, topChar, bottomChar, leftChar, rightChar);
 			
 			#if 1
 			batchReplace.array[replaceIndex].range = cursor.sel;
